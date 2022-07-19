4836075709c8f6bde9971ec4a42df3d983ffde17
Calvinwen10 <calvinwendi.002cw@gmail.com>
2022-07-19 23:14:18 +0700

    gallery

---
+++ Gallery/gallery.php
@@ -0,0 +1,83 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Bistecca | Gallery</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="gallery.css">
+</head>
+<body>
+    <?php require "../Navbar/navbar.php" ?>
+
+    <div class="gallery">
+        <p>Gallery</p>
+    </div>
+
+    <div class="grid-container">
+        <div class="container">
+            <img src="../images/gallery.png" alt="Avatar" class="image">
+            <div class="middle">
+              <div class="text">13 June 2002</div>
+            </div>
+        </div>
+        <div class="container">
+            <img src="../images/gallery1.png" alt="Avatar" class="image">
+            <div class="middle">
+              <div class="text">14 June 2002</div>
+            </div>
+        </div>
+        <div class="container">
+            <img src="../images/gallery.png" alt="Avatar" class="image">
+            <div class="middle">
+              <div class="text">15 June 2002</div>
+            </div>
+        </div>
+        <div class="container">
+            <img src="../images/gallery1.png" alt="Avatar" class="image">
+            <div class="middle">
+              <div class="text">16 June 2002</div>
+            </div>
+        </div>
+        <div class="container">
+            <img src="../images/gallery.png" alt="Avatar" class="image">
+            <div class="middle">
+              <div class="text">17 June 2002</div>
+            </div>
+        </div>
+        <div class="container">
+            <img src="../images/gallery1.png" alt="Avatar" class="image">
+            <div class="middle">
+              <div class="text">18 June 2002</div>
+            </div>
+        </div>
+        <div class="container">
+            <img src="../images/gallery.png" alt="Avatar" class="image">
+            <div class="middle">
+              <div class="text">19 June 2002</div>
+            </div>
+        </div>
+        <div class="container">
+            <img src="../images/gallery1.png" alt="Avatar" class="image">
+            <div class="middle">
+              <div class="text">23 June 2002</div>
+            </div>
+        </div>
+        <div class="container">
+            <img src="../images/gallery.png" alt="Avatar" class="image">
+            <div class="middle">
+              <div class="text">24 June 2002</div>
+            </div>
+        </div>
+    </div>
+
+    <div class="gallery">
+        <p></p>
+    </div>
+
+</body>
+</html>
+
+<script src="/script.js" defer></script>
+
