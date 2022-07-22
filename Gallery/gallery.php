b796179333d2fa7cff9a94478d9dfba6f59a21de
Calvinwen10 <calvinwendi.002cw@gmail.com>
2022-07-22 20:39:15 +0700

    gallery

--- Gallery/gallery.php
+++ Gallery/gallery.php
@@ -15,55 +15,55 @@
 
     <div class="grid-container">
         <div class="container">
-            <img src="../images/gallery.png" alt="Avatar" class="image">
+            <img src="../images/gallery10.jpg" alt="Avatar" class="image">
             <div class="middle">
               <div class="text">13 June 2002</div>
             </div>
         </div>
         <div class="container">
-            <img src="../images/gallery1.png" alt="Avatar" class="image">
+            <img src="../images/gallery9.png" alt="Avatar" class="image">
             <div class="middle">
               <div class="text">14 June 2002</div>
             </div>
         </div>
         <div class="container">
-            <img src="../images/gallery.png" alt="Avatar" class="image">
+            <img src="../images/gallery2.png" alt="Avatar" class="image">
             <div class="middle">
               <div class="text">15 June 2002</div>
             </div>
         </div>
         <div class="container">
-            <img src="../images/gallery1.png" alt="Avatar" class="image">
+            <img src="../images/gallery3.png" alt="Avatar" class="image">
             <div class="middle">
               <div class="text">16 June 2002</div>
             </div>
         </div>
         <div class="container">
-            <img src="../images/gallery.png" alt="Avatar" class="image">
+            <img src="../images/gallery4.jpg" alt="Avatar" class="image">
             <div class="middle">
               <div class="text">17 June 2002</div>
             </div>
         </div>
         <div class="container">
-            <img src="../images/gallery1.png" alt="Avatar" class="image">
+            <img src="../images/gallery5.jpg" alt="Avatar" class="image">
             <div class="middle">
               <div class="text">18 June 2002</div>
             </div>
         </div>
         <div class="container">
-            <img src="../images/gallery.png" alt="Avatar" class="image">
+            <img src="../images/gallery6.jpg" alt="Avatar" class="image">
             <div class="middle">
               <div class="text">19 June 2002</div>
             </div>
         </div>
         <div class="container">
-            <img src="../images/gallery1.png" alt="Avatar" class="image">
+            <img src="../images/gallery7.jpg" alt="Avatar" class="image">
             <div class="middle">
               <div class="text">23 June 2002</div>
             </div>
         </div>
         <div class="container">
-            <img src="../images/gallery.png" alt="Avatar" class="image">
+            <img src="../images/gallery9.jpg" alt="Avatar" class="image">
             <div class="middle">
               <div class="text">24 June 2002</div>
             </div>
